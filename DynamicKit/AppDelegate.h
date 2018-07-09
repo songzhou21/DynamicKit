df24a9a7836f889fda6a4f5b16b6d604a0056cb0
Song Zhou <zhousong1993@gmail.com>
2018-07-09 22:59:21 +0800

    Initial Commit

---
+++ DynamicKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicKit
+//
+//  Created by songzhou on 2018/7/9.
+//  Copyright © 2018年 songzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
